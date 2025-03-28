e14988910c872c9646976c128fb3c352c170c65d
NekhilBhaskar <nekhilb.ug23.cs@nitp.ac.in>
2025-03-28 11:02:50 +0530

    url done

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chatapp-7o3i.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
